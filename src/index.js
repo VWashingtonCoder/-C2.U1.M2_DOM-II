88261aabe06ee08315aa2ec0c1d4b2c4361f0200
kuraiokamiblaze <kuraiokamiblaze@penguin>
2022-02-09 09:17:36 -0700

    eventListener(#10)_resize added_MVP DONE

--- src/index.js
+++ src/index.js
@@ -4,49 +4,54 @@ import './less/index.less'
 //set up getters
 const links= document.querySelectorAll("a.nav-link");
 const buttons = document.querySelectorAll("div.btn");
-const contentImg = document.querySelectorAll("div.img-content");
 const containers = document.querySelectorAll("div.container");
 const subTitles = document.querySelectorAll("h4");
 
-
 // eventListeners(#1)_click
-links.forEach(link => link.addEventListener("click", evt => {
+  links.forEach(link => link.addEventListener("click", evt => {
     evt.target.style.color = "blue";
   }))
-  // eventListeners(#2&3)_mouseenter&mouseleave
-buttons.forEach(btn => btn.addEventListener("mouseenter", evt => {
+// eventListeners(#2&3)_mouseenter&mouseleave
+  buttons.forEach(btn => btn.addEventListener("mouseenter", evt => {
     evt.target.style.background = 'pink';
-}))
-buttons.forEach(btn => btn.addEventListener("mouseleave", evt => {
+  }))
+  buttons.forEach(btn => btn.addEventListener("mouseleave", evt => {
     evt.target.style.background = '';
-}))
+  }))
 // eventListener(#4)_scroll
-window.addEventListener('scroll', () => {
+  window.addEventListener('scroll', () => {
     const scrollable = document.documentElement.scrollHeight - window.innerHeight;
     const scrolled = window.scrollY;
     if (scrolled === scrollable){
         alert("Ready to Sign Up Now?")
     } 
-})
+  })
 // eventListeners(#5&#6)_mouseover&mouseout_
-links.forEach(link => link.addEventListener("mouseover", evt => {
+  links.forEach(link => link.addEventListener("mouseover", evt => {
     evt.target.style.color = "green";
   }))
 
   links.forEach(link => link.addEventListener("mouseout", evt => {
     evt.target.style.color = "";
   }))
-  // eventListener(#7)_dblclick+.stopPropagation() 
+// eventListener(#7)_dblclick+.stopPropagation() 
   containers.forEach(div => div.addEventListener("dblclick", evt => {
     evt.target.style.background = 'yellow';
     
-  }))
+  }));
   subTitles.forEach(h4 => h4.addEventListener("dblclick", evt => {
     evt.target.style.color = 'skyblue';
     evt.stopPropagation();
-  }))
-
+  }));
 // eventListener(#8)_auxclick
-containers.forEach(div => div.addEventListener("auxclick", evt => {
-  evt.target.style.background = '';
-}))
+  containers.forEach(div => div.addEventListener("auxclick", evt => {
+    evt.target.style.background = '';
+  }));
+// eventListener(#9)_load
+  window.addEventListener("load", evt=> {
+  alert("Welcome to the Fun Bus!!!!");
+  })
+// eventListener(#10)_resize
+  window.addEventListener("resize", evt => {
+    alert("It's Still The Fun Bus...Resized!");
+  })
