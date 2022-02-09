b9eee1cbff9c77010dea9d11efedf5eddc9b006a
kuraiokamiblaze <kuraiokamiblaze@penguin>
2022-02-08 22:57:08 -0700

    eventListeners(#5&#6)_mouseover&mouseout added

--- src/index.js
+++ src/index.js
@@ -16,12 +16,19 @@ buttons.forEach(btn => btn.addEventListener("mouseenter", evt => {
 buttons.forEach(btn => btn.addEventListener("mouseleave", evt => {
     evt.target.style.background = '';
 }))
-//eventListeners(#4)_scroll
+// eventListeners(#4)_scroll
 window.addEventListener('scroll', () => {
     const scrollable = document.documentElement.scrollHeight - window.innerHeight;
     const scrolled = window.scrollY;
-    
     if (scrolled === scrollable){
         alert("Ready to Sign Up Now?")
     } 
 })
+// eventListeners(#5)_
+links.forEach(link => link.addEventListener("mouseover", evt => {
+    evt.target.style.color = "green";
+  }))
+
+  links.forEach(link => link.addEventListener("mouseout", evt => {
+    evt.target.style.color = "";
+  }))
