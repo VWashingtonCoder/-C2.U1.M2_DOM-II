dcf53a8e97335d9eaf24c0dfde1beb8927340310
kuraiokamiblaze <kuraiokamiblaze@penguin>
2022-02-08 22:23:16 -0700

    eventListener #1_click added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './less/index.less'
 
 // Your code goes here!
+//set up getters
+const links= document.querySelectorAll("a.nav-link");
+const buttons = document.querySelectorAll("div.btn");
+
+// eventListeners(#1)_click
+links.forEach(link => link.addEventListener("click", evt => {
+    evt.target.style.color = "blue";
+  }))
