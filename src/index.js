afa39c18ec7417f96cba88289834253a12a80764
kuraiokamiblaze <kuraiokamiblaze@penguin>
2022-02-08 23:20:13 -0700

    eventListener(#7)_dblclick+.stopPropagation() added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './less/index.less'
 //set up getters
 const links= document.querySelectorAll("a.nav-link");
 const buttons = document.querySelectorAll("div.btn");
+const contentImg = document.querySelectorAll("div.img-content");
 
 // eventListeners(#1)_click
 links.forEach(link => link.addEventListener("click", evt => {
@@ -16,7 +17,7 @@ buttons.forEach(btn => btn.addEventListener("mouseenter", evt => {
 buttons.forEach(btn => btn.addEventListener("mouseleave", evt => {
     evt.target.style.background = '';
 }))
-// eventListeners(#4)_scroll
+// eventListener(#4)_scroll
 window.addEventListener('scroll', () => {
     const scrollable = document.documentElement.scrollHeight - window.innerHeight;
     const scrolled = window.scrollY;
@@ -24,11 +25,17 @@ window.addEventListener('scroll', () => {
         alert("Ready to Sign Up Now?")
     } 
 })
-// eventListeners(#5)_
+// eventListeners(#5&#6)_mouseover&mouseout_
 links.forEach(link => link.addEventListener("mouseover", evt => {
     evt.target.style.color = "green";
   }))
 
   links.forEach(link => link.addEventListener("mouseout", evt => {
     evt.target.style.color = "";
+  }))
+
+  // event
+  document.querySelectorAll("*").forEach(elem => elem.addEventListener("dblclick", evt => {
+    console.log(`event target: ${evt.target}`);
+    evt.stopPropagation()
   }))
